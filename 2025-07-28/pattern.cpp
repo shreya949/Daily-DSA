5c138d2208bf000501f6566117f4f6c772396235
shreya949 <shreya.joshi949@gmail.com>
2025-07-29 22:12:24 +0530

    modified pattern

--- 2025-07-28/pattern.cpp
+++ 2025-07-28/pattern.cpp
@@ -10,11 +10,22 @@ void nForest2(int n) {
 		cout<<endl;
 	}
 }
+void nForest3(int n) {
+    char ch='A';
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<ch<<" ";
+            ch=ch+1;
+		}
+		cout<<endl;
+	}
+}
 int main() {
     int n;
     cout << "Enter the number of rows: ";
     cin >> n;
-    nForest2(n);
+    //nForest2(n);
+    nForest3(n);
     return 0;
 }
 
@@ -22,3 +33,10 @@ int main() {
 // 1 2 3 
 // 4 5 6
 // 7 8 9
+
+// Enter the number of rows: 5
+// A B C D E 
+// F G H I J
+// K L M N O
+// P Q R S T
+// U V W X Y
